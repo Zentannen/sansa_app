fb2156d904386aa08684a9ef25adccf1477cbb33
Zentannen <56276282+Zentannen@users.noreply.github.com>
2021-06-25 17:25:58 +0200

    Some basic theming

--- src/screens/screen.js
+++ src/screens/screen.js
@@ -1,6 +1,19 @@
 import React,{useState} from 'react'
-import {Paper} from '@material-ui/core'
-export default function Screen(){
+import {Paper,AppBar,Toolbar,makeStyles,IconButton,Drawer,Typography,Divider} from '@material-ui/core'
+import {Menu,Close} from '@material-ui/icons'
+const useStyles = makeStyles((theme) => ({
+    root:{
+        width:'100vw',
+        height:'100vh',
+        overflow:"scroll"
+    },
+    AppBar: {
+      flexGrow: 1,
+    },
+  }));
+
+export default function Screen({desktop}){
+    const classes = useStyles();
     const [screen,setScreen] = useState(1)
     const changeScreen = (value) => {
         console.log(typeof value === 'number'&&value!==0&&value<=5)
@@ -8,9 +21,43 @@ export default function Screen(){
             setScreen(value)
         }
     }
-    const [test,setTest] = useState(0)
+    const [sidebarOpen,setSidebarOpen]=useState(false)
+    const changeSidebarOpen = (boolean) => {
+        setSidebarOpen(boolean)
+    }
     return(
         <div>
+            <AppBar>
+                <Toolbar>
+                    <IconButton onClick={()=>changeSidebarOpen(true)}>
+                        <Menu/>
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+            <Drawer anchor="left" open={sidebarOpen}>
+                <div style={{maxWidth:desktop?'90vw':'',width:desktop?'':'100vw', minWidth:'30vw',height:'100vh'}}>
+                    <Toolbar style={{display:'flex',alignItems:'center',justifyContent:'space-between'}}>
+                        <Typography>SIA</Typography>
+                        <IconButton onClick={()=>changeSidebarOpen(false)}>
+                            <Close/>
+                        </IconButton>
+                    </Toolbar>
+                    <Divider/>
+                    <Toolbar>
+                        <Typography>What are space winds?</Typography>
+                    </Toolbar>
+                    <Divider/>
+                    <Toolbar>
+                        <Typography>What are space winds?</Typography>
+                    </Toolbar>
+                    <Divider/>
+                    <Toolbar>
+                        <Typography>What are space winds?</Typography>
+                    </Toolbar>
+                    <Divider/>
+                </div>
+            </Drawer>
+            <Toolbar/>
             <Paper>asd</Paper>
         </div>
     )
