fc8c0435dc136c285d10a503136e956489918136
Zentannen <56276282+Zentannen@users.noreply.github.com>
2021-06-25 18:55:35 +0200

    Addition of multiple parts

--- src/screens/screen.js
+++ src/screens/screen.js
@@ -1,6 +1,6 @@
 import React,{useState} from 'react'
-import {Paper,AppBar,Toolbar,makeStyles,IconButton,Drawer,Typography,Divider} from '@material-ui/core'
-import {Menu,Close} from '@material-ui/icons'
+import {Paper,AppBar,Toolbar,makeStyles,IconButton,Drawer,Typography,Divider,Badge,Dialog,Slide, Link} from '@material-ui/core'
+import {Menu,Close,Notifications,BlurOn,Flare,Book} from '@material-ui/icons'
 const useStyles = makeStyles((theme) => ({
     root:{
         width:'100vw',
@@ -10,55 +10,123 @@ const useStyles = makeStyles((theme) => ({
     AppBar: {
       flexGrow: 1,
     },
-  }));
-
-export default function Screen({desktop}){
-    const classes = useStyles();
-    const [screen,setScreen] = useState(1)
-    const changeScreen = (value) => {
-        console.log(typeof value === 'number'&&value!==0&&value<=5)
-        if(typeof value === 'number'&&value!==0&&value<=5){
-            setScreen(value)
-        }
+    toolbar: {
+        display:'flex',
+        alignItems:'center',
+        justifyContent:'space-between'
+    },
+    typography:{
+        fontWeight:'600'
     }
+  }));
+  const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+  });
+
+export default function Screen({desktop,theme}){
+    const classes = useStyles();
+    const [screen,setScreen] = useState(0)
     const [sidebarOpen,setSidebarOpen]=useState(false)
+    const [notificationScreenOpen,setNotificationScreenOpen]=useState(false)
     const changeSidebarOpen = (boolean) => {
         setSidebarOpen(boolean)
     }
+    const changeNotificationScreenOpen = (boolean) => {
+        setNotificationScreenOpen(boolean)
+    }
+    const changeScreen = (value) => {
+        if(sidebarOpen===true){
+            setSidebarOpen(false)
+        }
+        if(value===3){
+            changeNotificationScreenOpen(true)
+        }
+        setScreen(value)
+    }
+    const sidebarList = [
+        {
+            title:'Forcast',
+            icon:<Flare/>
+        },
+        {
+            title:'Conditions',
+            icon:<BlurOn/>
+        },
+        {
+            title:'Other resources',
+            icon:<Book/>
+        },
+    ]
     return(
-        <div>
+        <div className={classes.root}>
             <AppBar>
-                <Toolbar>
-                    <IconButton onClick={()=>changeSidebarOpen(true)}>
+                <Paper elevation={0} square>
+                <Toolbar className={classes.toolbar}>
+                    <IconButton onClick={()=>changeSidebarOpen(true)} color="secondary">
                         <Menu/>
                     </IconButton>
+                    <IconButton onClick={()=>changeScreen(3)} color="secondary">
+                        <Badge badgeContent={4} color="secondary">
+                            <Notifications/>
+                        </Badge>
+                    </IconButton>
                 </Toolbar>
+                </Paper>
             </AppBar>
             <Drawer anchor="left" open={sidebarOpen}>
                 <div style={{maxWidth:desktop?'90vw':'',width:desktop?'':'100vw', minWidth:'30vw',height:'100vh'}}>
-                    <Toolbar style={{display:'flex',alignItems:'center',justifyContent:'space-between'}}>
+                    <Toolbar className={classes.toolbar}>
                         <Typography>SIA</Typography>
-                        <IconButton onClick={()=>changeSidebarOpen(false)}>
+                        <IconButton onClick={()=>changeSidebarOpen(false)} color="secondary">
                             <Close/>
                         </IconButton>
                     </Toolbar>
                     <Divider/>
-                    <Toolbar>
-                        <Typography>What are space winds?</Typography>
-                    </Toolbar>
-                    <Divider/>
-                    <Toolbar>
-                        <Typography>What are space winds?</Typography>
-                    </Toolbar>
-                    <Divider/>
-                    <Toolbar>
-                        <Typography>What are space winds?</Typography>
-                    </Toolbar>
-                    <Divider/>
+                    {sidebarList.map((i,ii)=>
+                        // console.log(ii == screen);
+                        (
+                        <>
+                            <div style={{backgroundColor:screen==(ii)?theme.theme?'#00000050':'#eee':'',transition:'0.5s'}} onClick={()=>changeScreen(ii)}>
+                                <Toolbar className={classes.toolbar} style={{justifyContent:'flex-start'}}>
+                                    <IconButton color="secondary">
+                                        {i.icon}
+                                    </IconButton>
+                                    <Typography style={{marginLeft:'7.5px'}}>{i.title}</Typography>
+                                </Toolbar>
+                            </div>
+                            <Divider variant="middle"/>
+                        </>
+                    ))}
                 </div>
             </Drawer>
+            <Dialog 
+                fullScreen 
+                open={notificationScreenOpen} 
+                TransitionComponent={Transition}
+                keepMounted
+                >
+                <div className={classes.root} style={{backgroundColor:theme.theme?'#00000050':'#eee'}}>
+                    <Paper elevation={0} square>
+                        <Toolbar className={classes.toolbar} position='absolute'>
+                            <Typography>Noftications</Typography>
+                            <IconButton onClick={()=>changeNotificationScreenOpen(false)} color="secondary">
+                                <Close/>
+                            </IconButton>
+                        </Toolbar>
+                    </Paper>
+                    <Divider/>
+                    <div style={{width:'90vw',margin:'5vh 10vw'}}>
+                        <Typography className={classes.typography}>Solar flare</Typography>
+                        <Paper style={{width:'80vw',minHeight:'20vh',padding:'2vh 2vw',margin:'1vh 0'}}>
+                            {`A solar flare was detected at at Thu Jan 26 2017 11:00:00 GMT+1100, and should reach earth within the next 20 minutes. A solar flare can seriously damage electronics. One can help avoid these damages by unplugging devices or contacting the relative experts.`} Read <Link color="secondary">here</Link> for more.
+                        </Paper>
+                    </div>
+                </div>
+            </Dialog>
             <Toolbar/>
-            <Paper>asd</Paper>
+            <>
+            {}
+            </>
         </div>
     )
 }
